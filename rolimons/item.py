a789a55561f88bf6b614f8eaca312c8df6b99d4a
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-09-03 11:07:59 -0400

    type hints

--- rolimons/item.py
+++ rolimons/item.py
@@ -4,84 +4,6 @@ from bs4 import BeautifulSoup
 from .exceptions import RateLimitError
 from .exceptions import InvalidItemDetails
 
-
-class item:
-    def __str__(self):
-        return self.name
-
-    def __init__(self, id: int, itemdetails: dict = None):
-        if itemdetails is None:
-            itemdetails = requests.get(
-                'https://api.rolimons.com/items/v1/itemdetails').json()
-            if not itemdetails['success']:
-                raise RateLimitError(
-                    'Rate limit exceeded for this itemdetails endpoint')
-
-        try:
-            item = itemdetails['items'][str(id)]
-            self.id: int = id
-            self.name: str = item[0].strip()
-            self.acronym: str = item[1] if item[1] != '' else None
-            self.rap: int = item[2]
-            self.value: int = item[3] if item[3] != -1 else item[2]
-            self.default_value: int = item[4]
-            self.demand: int = item[5] if item[5] != -1 else None
-            self.trend: int = item[6] if item[6] != -1 else None
-            self.projected: bool = 1 in [item[7]]
-            self.hyped: bool = 1 in [item[8]]
-            self.rare: bool = 1 in [item[9]]
-        except KeyError:
-            raise InvalidItemDetails(
-                'Invalid itemdetails dictionary passed in')
-
-    def get_sales_data(self):
-        request = requests.get(f'https://www.rolimons.com/item/{self.id}')
-        soup = BeautifulSoup(request.text, 'html.parser')
-        tags = [tag.get_text() for tag in soup.find_all(
-            'h5', attrs={'class': 'card-title mb-1 text-light text-truncate stat-data'})]
-        tags = [tags[3], tags[5], tags[11], tags[10], soup.find_all(
-            'div', attrs={'class': 'value-stat-data'})[0].get_text()]
-        return Sales(tags)
-
-    def get_recent_sales(self) -> list[Sale]:
-        request = requests.get(f'https://www.rolimons.com/itemsales/{self.id}')
-        soup = BeautifulSoup(request.text, 'html.parser')
-        timestamps = [tag.get_text() for tag in soup.find_all(
-            'div', attrs={'class': 'activity_entry_timestamp my-auto'})]
-        meta = soup.find_all(
-            'div', attrs={'class': 'activity_stats_section d-flex justify-content-around'})
-        sales = []
-        for i, timestamp in enumerate(timestamps):
-            sales_price = BeautifulSoup(str(meta[i]), 'html.parser').find_all(
-                'div', attrs={'class': 'pl-1'})[0].get_text()
-            sales_rap_change = [tag.get_text().replace('\n', '') for tag in BeautifulSoup(
-                str(meta[i]), 'html.parser').find_all('div', attrs={'class': 'activity_stat_data'})]
-            sales.append(
-                Sale([timestamp, sales_price, sales_rap_change[0], sales_rap_change[1]]))
-        return sales
-
-    def get_ownership_data(self):
-        request = requests.get(f'https://www.rolimons.com/item/{self.id}')
-        soup = BeautifulSoup(request.text, 'html.parser')
-        tags = [tag.get_text() for tag in soup.find_all(
-            'div', attrs={'class': 'value-stat-data'})]
-        return Ownership(tags)
-
-    def get_owner_data(self):
-        def index_segment(start, end, data):
-            i = data.index(start)
-            while 1:
-                if data[i:i + len(end)] == end:
-                    break
-                i += 1
-            return data[data.index(start):i].replace(start, '').strip()
-
-        request = requests.get(f'https://www.rolimons.com/item/{self.id}').text
-        data = json.loads(index_segment('var all_copies_data                = ',
-                          '      var all_copies_data_count ', request)[:-1])
-        return ItemOwners(data)
-
-
 class ItemOwners:
     def __init__(self, data):
         self.num_copies: int = int(data['num_copies'])
@@ -134,3 +56,80 @@ class Owner:
         self.uaid: int = obj.uaids[i]
         self.updated: int = int(obj.updated[i])
         self.premium: bool = True if obj.owner_membership[i] else False
+
+
+class item:
+    def __str__(self) -> str:
+        return self.name
+
+    def __init__(self, id: int, itemdetails: dict = None) -> None:
+        if itemdetails is None:
+            itemdetails = requests.get(
+                'https://api.rolimons.com/items/v1/itemdetails').json()
+            if not itemdetails['success']:
+                raise RateLimitError(
+                    'Rate limit exceeded for this itemdetails endpoint')
+
+        try:
+            item = itemdetails['items'][str(id)]
+            self.id: int = id
+            self.name: str = item[0].strip()
+            self.acronym: str = item[1] if item[1] != '' else None
+            self.rap: int = item[2]
+            self.value: int = item[3] if item[3] != -1 else item[2]
+            self.default_value: int = item[4]
+            self.demand: int = item[5] if item[5] != -1 else None
+            self.trend: int = item[6] if item[6] != -1 else None
+            self.projected: bool = 1 in [item[7]]
+            self.hyped: bool = 1 in [item[8]]
+            self.rare: bool = 1 in [item[9]]
+        except KeyError:
+            raise InvalidItemDetails(
+                'Invalid itemdetails dictionary passed in')
+
+    def get_sales_data(self) -> Sales:
+        request = requests.get(f'https://www.rolimons.com/item/{self.id}')
+        soup = BeautifulSoup(request.text, 'html.parser')
+        tags = [tag.get_text() for tag in soup.find_all(
+            'h5', attrs={'class': 'card-title mb-1 text-light text-truncate stat-data'})]
+        tags = [tags[3], tags[5], tags[11], tags[10], soup.find_all(
+            'div', attrs={'class': 'value-stat-data'})[0].get_text()]
+        return Sales(tags)
+
+    def get_recent_sales(self) -> list[Sale]:
+        request = requests.get(f'https://www.rolimons.com/itemsales/{self.id}')
+        soup = BeautifulSoup(request.text, 'html.parser')
+        timestamps = [tag.get_text() for tag in soup.find_all(
+            'div', attrs={'class': 'activity_entry_timestamp my-auto'})]
+        meta = soup.find_all(
+            'div', attrs={'class': 'activity_stats_section d-flex justify-content-around'})
+        sales = []
+        for i, timestamp in enumerate(timestamps):
+            sales_price = BeautifulSoup(str(meta[i]), 'html.parser').find_all(
+                'div', attrs={'class': 'pl-1'})[0].get_text()
+            sales_rap_change = [tag.get_text().replace('\n', '') for tag in BeautifulSoup(
+                str(meta[i]), 'html.parser').find_all('div', attrs={'class': 'activity_stat_data'})]
+            sales.append(
+                Sale([timestamp, sales_price, sales_rap_change[0], sales_rap_change[1]]))
+        return sales
+
+    def get_ownership_data(self) -> Ownership:
+        request = requests.get(f'https://www.rolimons.com/item/{self.id}')
+        soup = BeautifulSoup(request.text, 'html.parser')
+        tags = [tag.get_text() for tag in soup.find_all(
+            'div', attrs={'class': 'value-stat-data'})]
+        return Ownership(tags)
+
+    def get_owner_data(self) -> ItemOwners:
+        def index_segment(start, end, data):
+            i = data.index(start)
+            while 1:
+                if data[i:i + len(end)] == end:
+                    break
+                i += 1
+            return data[data.index(start):i].replace(start, '').strip()
+
+        request = requests.get(f'https://www.rolimons.com/item/{self.id}').text
+        data = json.loads(index_segment('var all_copies_data                = ',
+                          '      var all_copies_data_count ', request)[:-1])
+        return ItemOwners(data)
